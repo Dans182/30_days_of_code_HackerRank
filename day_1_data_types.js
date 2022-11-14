edd6bc529941f4a58d827506386339907e11d1db
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-11-14 23:40:19 +0100

    added_day_1

---
+++ day_1_data_types.js
@@ -0,0 +1,16 @@
+// Declare second integer, double, and String variables.
+let integer;
+let double;
+let string;
+// Read and save an integer, double, and String to your variables.
+integer = parseInt(readLine());
+double = parseFloat(readLine());
+string = readLine();
+// Print the sum of both integer variables on a new line.
+console.log(i + integer);
+// Print the sum of the double variables on a new line.
+console.log(parseFloat(d + double).toFixed(1));
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+console.log(s + string);
+// The 's' variable above should be printed first.
